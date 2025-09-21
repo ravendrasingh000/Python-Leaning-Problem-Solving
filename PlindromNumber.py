f421fd3df264dbef249e79dec0c754bb41719bf0
Ravendra Singh <ravendra24@navgurukul.org>
2025-09-21 16:52:23 +0530

    Palindrome Number

--- PlindromNumber.py
+++ PlindromNumber.py
@@ -1,2 +1,16 @@
 num = 12321
 
+renum = num
+rv = 0
+digit = 0
+
+while num > 0:
+    digit = num%10
+    rv = rv*10+digit
+    num = num//10
+
+if renum == rv:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+
