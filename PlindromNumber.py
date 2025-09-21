d085dbbe161cff93bac52c54a96a5e662d54b9db
Ravendra Singh <ravendra24@navgurukul.org>
2025-09-21 17:08:11 +0530

    Short way to solve Palindrom

--- PlindromNumber.py
+++ PlindromNumber.py
@@ -1,16 +1,28 @@
-num = 12321
+# num = 12321
 
-renum = num
-rv = 0
-digit = 0
+# renum = num
+# rv = 0
+# digit = 0
 
-while num > 0:
-    digit = num%10
-    rv = rv*10+digit
-    num = num//10
+# while num > 0:
+#     digit = num%10
+#     rv = rv*10+digit
+#     num = num//10
 
-if renum == rv:
-    print("Palindrome")
+# if renum == rv:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+
+
+
+
+
+n = 123454321
+
+num = str(n)
+
+if num == num[::-1]:
+    print("Palindrom")
 else:
     print("Not Palindrome")
-
