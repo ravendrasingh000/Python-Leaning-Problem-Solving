978c952606ab639f646940452865d435dcdae292
Ravendra Singh <ravendra24@navgurukul.org>
2025-09-24 16:18:05 +0530

    Anagram in detailed way

--- Anagram.py
+++ Anagram.py
@@ -1,12 +1,36 @@
 # Anagram
 
+# str1 = "listen"
+# str2 = "silent"
+
+# sstr1 = sorted(str1.lower())
+# sstr2 = sorted(str2.lower())
+
+# if sstr1 == sstr2:
+#     print("Anagram")
+# else:
+#     print("Not Anagram")
+
+
+# ..............................................................................................
+# in detailed way
+
 str1 = "listen"
 str2 = "silent"
 
-sstr1 = sorted(str1.lower())
-sstr2 = sorted(str2.lower())
+f = str1.lower()
+s = str2.lower()
+count = 0 
 
-if sstr1 == sstr2:
+for ch in f:
+    if ch in s:
+        count += 1
+        s = s.replace(ch, "", 1)
+    else:
+        print("Not Anagram")
+        break
+
+if count == len(f):
     print("Anagram")
 else:
-    print("Not Anagram")
+    print("Not Anagram")         
